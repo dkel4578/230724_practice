6e9e43ba99efa54684bf9a32220036c751671a4a
student <dkel4578@naver.com>
2023-07-24 10:58:01 +0900

    1. 첫 spring boot 세팅 1

---
+++ java-spring/src/main/java/com/thejoeun/practicd1/boilerplate1/Boilerplate1Application.java
@@ -0,0 +1,13 @@
+package com.thejoeun.practicd1.boilerplate1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boilerplate1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Boilerplate1Application.class, args);
+	}
+
+}
