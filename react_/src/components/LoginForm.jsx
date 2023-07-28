446163f248a45153fc6b56eab700273937235f50
student <dkel4578@naver.com>
2023-07-28 09:48:22 +0900

    login 페이지 구현

--- react_/src/components/LoginForm.jsx
+++ react_/src/components/LoginForm.jsx
@@ -1,7 +1,41 @@
 import React, { useState, useRef, useContext, useEffect } from 'react';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useNavigate } from 'react-router-dom';
 
 function LoginForm () {
+  let navigate = useNavigate();
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const submitHandler = (e) =>{
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+
+    const jsonContent = process.env.REACT_APP_API_JSON_CONTENT;
+
+    axios.post('/api/auth/login', {
+      email: email,
+      password: password,
+    }, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then(response => {
+      if (response && response.status === 200) {
+        alert('로그인 완료');
+        navigate('/');
+      } else {
+        alert('로그인 실패');
+      }
+    })
+    .catch(error => {
+      console.error('Error during signup:', error);
+      alert('로그인 실패');
+    });
+
+  }
   return (
     <section className="d-flex vh-100">
       <div className="container-fluid row justify-content-center align-content-center">
@@ -9,19 +43,19 @@ function LoginForm () {
           <div className="card-body p-5 text-center">
             <h2 className="text-white">LOGIN</h2>
             <p className="text-white-50 mt-2 mb-5">서비스를 사용하려면 로그인을 해주세요!</p>
-            <div class = "mb-2">
-              <form action="/login" method="POST">
+            <div className = "mb-2">
+              <form onSubmit={submitHandler}>
                 <div className="mb-3">
                   <label className="form-label text-white">Email address</label>
-                  <input type="email" className="form-control" name="username" />
+                  <input type="email" className="form-control" name="username" required ref={emailRef}/>
                 </div>
                 <div className="mb-3">
                   <label className="form-label text-white">Password</label>
-                  <input type="password" className="form-control" name="password" />
+                  <input type="password" className="form-control" name="password" required ref={passwordRef}/>
                 </div>
                 <button type="submit" className="btn btn-primary">Submit</button>
               </form>
-              <button type="button" className="btn btn-secondary mt-3" onclick="location.href='/signup'">회원가입</button>
+              <button type="button" className="btn btn-secondary mt-3">회원가입</button>
             </div>
           </div>
         </div>
--- react_/src/components/SignUpForm.jsx
+++ react_/src/components/SignUpForm.jsx
@@ -1,7 +1,46 @@
 import React, { useState, useRef, useContext, useEffect } from 'react';
-
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function SignUpForm () {
+  let navigate = useNavigate();
+
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+  const nicknameRef = useRef(null);
+
+  const submitHandler = (e) =>{
+    e.preventDefault();
+    console.log('따닥')
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    const nickname = nicknameRef.current.value;
+
+    const jsonContent = process.env.REACT_APP_API_JSON_CONTENT;
+
+    axios.post('/api/auth/signup', {
+      email: email,
+      password: password,
+      nickname: nickname,
+    }, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then(response => {
+      if (response && response.status === 200) {
+        alert('회원가입 완료');
+        navigate('/login');
+      } else {
+        alert('회원등록 실패');
+      }
+    })
+    .catch(error => {
+      console.error('Error during signup:', error);
+      alert('회원등록 실패');
+    });
+  }
+  
   return (
     <>
       <section className="d-flex vh-100">
@@ -10,23 +49,24 @@ function SignUpForm () {
           <div className="card-body p-5 text-center">
             <h2 className="text-white">SIGN UP</h2>
             <p className="text-white-50 mt-2 mb-5">서비스 사용을 위한 회원 가입</p>
-            <div class = "mb-2">
+            <div className = "mb-2">
               {/* <form th:action="@{/user}" method="POST"> */}
                 {/* <input type="hidden" th:name="${_csrf?.parameterName}" th:value="${_csrf?.token}" /> */}
+                <form onSubmit={submitHandler}>
                 <div className="mb-3">
                   <label className="form-label text-white">Email address</label>
-                  <input type="email" className="form-control" name="email" />
+                  <input type="email" className="form-control" name="email" id="email" required ref={emailRef}/>
                 </div>
                 <div className="mb-3">
                   <label className="form-label text-white">Password</label>
-                  <input type="password" className="form-control" name="password" />
+                  <input type="password" className="form-control" name="password" id="password" required ref={passwordRef}/>
                 </div>
                 <div className="mb-3">
                   <label className="form-label text-white">Nickname</label>
-                  <input type="text" className="form-control" name="nickname" />
+                  <input type="text" className="form-control" name="nickname" id="nickname" required ref={nicknameRef}/>
                 </div>
-
                 <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
               {/* </form> */}
             </div>
           </div>
